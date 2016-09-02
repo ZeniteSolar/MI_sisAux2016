6422c019dfd7fcba766b63ad1b028f1d73690971
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2016-09-02 18:55:56 -0300

    Update main.c
    Implementado UART

--- Software/main.c
+++ Software/main.c
@@ -1,97 +1,36 @@
-/*
- * MI_sisAux2016.c
- *
- * Created: 08/7/2016 13:25:59
- *  Author: Natan Ogliari
- *	Equipe Zênite Solar 
- */
+/**
+* @file main.c
+* @brief MI_sisAux2016.c
+*
+* @author Natan Ogliari
+*
+* @date 7/08/2016
+*/
+
 #define F_CPU 16000000UL
-#include <avr/io.h>
 #include <avr/pgmspace.h>
+#include "globalDefines.h"
+//#include <util/delay.h>
+//#include "avr/wdt.h"
+#include "ATmega328.h"
+//#include <avr/io.h>
+#include <string.h>
 #include "can.h"
-//#include "avr/wdt.h"//watchdog
+
+#define aciBomba1	PD3			//! Definição de pino de saida
+#define aciBomba2	PD4			//! Definição de pino de saida
+#define aciMPPT	    PD5			//! Definição de pino de saida
+#define aux1	    PD6			//! Definição de pino de saida
+#define aux2	    PD7			//! Definição de pino de saida
+#define INICIODOPACOTE		'@'
+#define FINALDOPACOTE		'*'
+#define POT_MODE		2 //!<APAGAR*****
+#define SERIAL_MODE		1 //!<APAGAR*********
+#define SETWORDSIZE 		8  // Number of bits of word to set parameter, used in the comunication with PC
+#define GETWORDSIZE 		4  // Number of bits of word to get parameter, used in the comunication with PC
 
 
-
-#define aciBomba1	PD3
-#define aciBomba2	PD4
-#define aciMPPT	    PD5
-#define aux1	    PD6
-#define aux2	    PD7
-
-// -----------------------------------------------------------------------------
-/** Set filters and masks.
- *
- * The filters are divided in two groups:
- *
- * Group 0: Filter 0 and 1 with corresponding mask 0.
- * Group 1: Filter 2, 3, 4 and 5 with corresponding mask 1.
- *
- * If a group mask is set to 0, the group will receive all messages.
- *
- * If you want to receive ONLY 11 bit identifiers, set your filters
- * and masks as follows:
- *
- *	uint8_t can_filter[] PROGMEM = {
- *		// Group 0
- *		MCP2515_FILTER(0),				// Filter 0
- *		MCP2515_FILTER(0),				// Filter 1
- *		
- *		// Group 1
- *		MCP2515_FILTER(0),				// Filter 2
- *		MCP2515_FILTER(0),				// Filter 3
- *		MCP2515_FILTER(0),				// Filter 4
- *		MCP2515_FILTER(0),				// Filter 5
- *		
- *		MCP2515_FILTER(0),				// Mask 0 (for group 0)
- *		MCP2515_FILTER(0),				// Mask 1 (for group 1)
- *	};
- *
- *
- * If you want to receive ONLY 29 bit identifiers, set your filters
- * and masks as follows:
- *
- * \code
- *	uint8_t can_filter[] PROGMEM = {
- *		// Group 0
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 0
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 1
- *		
- *		// Group 1
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 2
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 3
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 4
- *		MCP2515_FILTER_EXTENDED(0),		// Filter 5
- *		
- *		MCP2515_FILTER_EXTENDED(0),		// Mask 0 (for group 0)
- *		MCP2515_FILTER_EXTENDED(0),		// Mask 1 (for group 1)
- *	};
- * \endcode
- *
- * If you want to receive both 11 and 29 bit identifiers, set your filters
- * and masks as follows:
- */
-const uint8_t can_filter[] PROGMEM = 
-{
-	// Group 0
-	//MCP2515_FILTER(0),				// Filter 0
-	//MCP2515_FILTER(0),				// Filter 1
-	
-	// Group 1
-	MCP2515_FILTER(1),				// Mask 0 (for group 0
-};
-// You can receive 11 bit identifiers with either group 0 or 1.
-
-
-// -----------------------------------------------------------------------------
-// Main loop for receiving and sending messages.
-
-int main(void)
-{
-	
-	//wdt_enable(WDTO_2S); //habilita watchdog em 2s
-	
-	typedef union estados_t{
+typedef union estados_t{	//!cria uma estrutura para ser enviado no pacote.
 		struct {
 			unsigned char estBomba1 : 1;
 			unsigned char estBomba2 : 1;
@@ -102,54 +41,298 @@ int main(void)
 		unsigned char todas;
 	} estados_t;
 
-	volatile estados_t estados;
+volatile estados_t estados;		//<!Define estrutura como dado Volatile.
+
+// Bit field flags
+struct system_flags
+{
+	uint8 warning		: 1;
+	uint8 erro 			: 1;
+	uint8 mode			: 2;//modos de operação, definem quem controla o acionamento
+	uint8 on			: 1;
+	uint8 dms	    	: 1;
+	uint8 				: 0;//completa a variavel
+};
+typedef struct system_flags flags_t;
+flags_t flags;
+//volatile struct system_flags flags_t;
+
+const	uint8_t can_filter[] PROGMEM;		//!< Declara a função de filtro do canBus
+void stringTransmit(char* texto);
+void esvaziaBuffer();
+
+/// @Brief Função principal.
+/// here...
+int main(void)
+{
+	_delay_ms(1000);
+	// VARIAVEIS LOCAIS;
+	char frameData[50];
+	uint8 frameIndex = 0;
+	char recebido[100] = "";
+	char msgToSend[8] = "";
+	uint8 pos =  0;
+	flags.mode = POT_MODE;
 	
+	DDRD = (1 << aciBomba1) || (1 << aciBomba2) || (1 << aciMPPT) || (1 << aux1) || (1 << aux2);
+ 	//wdt_enable(WDTO_2S); //!<habilita watchdog em 2s
+	estados.todas = 0;
 	// Initialize MCP2515
-	can_init(BITRATE_125_KBPS);//define velocidade da comunicação
+	can_init(BITRATE_125_KBPS);		//!<Define a velocidade de comunicação.
 	
 	// Load filters and masks
 	can_static_filter(can_filter);
+		
+	can_t msg;		//!Cria o pacote a ser enviado.
 	
+	msg.id = 0x000;
+	//msg.flags.rtr = 0;
 	
+	msg.length = 5;			//!Define o tamanho do pacote a ser enviado.
 	
-	
-	// Create a test messsage
-	can_t msg;
-	
-	msg.id = 0x123;
-	msg.flags.rtr = 0;
-	//msg.flags.extended = 0;
-	
-	msg.length = 5;
-	msg.data[0] = estados.estBomba1;
-	msg.data[1] = estados.estBomba2;
-	msg.data[2] = estados.estMPPT;
-	msg.data[3] = estados.estAux1;
-	msg.data[3] = estados.estAux2;
-	
-	// Send the message
-	can_send_message(&msg);
+	//se estiver no modo Serial configura a usart
+	if (flags.mode == SERIAL_MODE)
+	{
+		// CONFIGURA A USART
+		usartConfig(USART_MODE_ASYNCHRONOUS,USART_BAUD_9600 ,USART_DATA_BITS_8,USART_PARITY_NONE,USART_STOP_BIT_SINGLE);
+		usartEnableReceiver();
+		usartEnableTransmitter();
+		usartActivateReceptionCompleteInterrupt();
+	}
 	
 	while (1)
 	{
-		//wdt_reset(); //diz que esta ok para watchdog
+		//wdt_reset(); //!diz que esta ok para watchdog
 		
-		// Check if a new messag was received
+		if ((1<<aciBomba1)&&PORTD)//!Realiza a leitura Digital na porta PD3
+		{
+			estados.estBomba1 = !estados.estBomba1;
+		}
+			
+		if ((1<<aciBomba2)&&PORTD)//!Realiza a leitura Digital na porta PD4
+		{
+			estados.estBomba2 = !estados.estBomba2;
+		}
+		
+		if ((1<<aciMPPT)&&PORTD)//!Realiza a leitura Digital na porta PD5
+		{
+			estados.estMPPT = !estados.estMPPT;
+		}
+		
+		if ((1<<aux1)&&PORTD)//!Realiza a leitura Digital na porta PD6
+		{
+			estados.estAux1 = !estados.estAux1;
+		}
+		
+		if ((1<<aux2)&&PORTD)//!Realiza a leitura Digital na porta PD7
+		{
+			estados.estAux2 = !estados.estAux2;
+		}
+		//!<Empacota os dados a serem enviados.
+		msg.data[0] = estados.estBomba1;
+		msg.data[1] = estados.estBomba2;
+		msg.data[2] =   estados.estMPPT;
+		msg.data[3] =   estados.estAux1;
+		msg.data[4] =   estados.estAux2; 
+		 
+	    can_send_message(&msg);		//!Envia um o pacote
+		 
+		//!Check if a new messag was received
 		if (can_check_message())
 		{
 			can_t msg;
 			
-			// Try to read the message
+			//!Try to read the message
 			if (can_get_message(&msg))
 			{
-				// If we received a message resend it with a different id
 				
-				
-				// Send the new message
+				//!Send the new message
 				can_send_message(&msg);
 			}
 		}
-	//wdt_reset(); //diz que esta ok para watchdog	
+		
+		
+		if(flags.mode == SERIAL_MODE)
+		{
+			while(!usartIsReceiverBufferEmpty())
+			{
+				frameData[frameIndex++] = usartGetDataFromReceiverBuffer();
+				if ((frameData[frameIndex-1] == FINALDOPACOTE))
+				{//se esta no final da palavra
+					if(frameData[0] == INICIODOPACOTE )
+					{//verifica se o inicio da palavra esta correto
+						strcpy(recebido,frameData);
+						pos = (recebido[2]-48) + (recebido[1] - 48)*10;
+						if(frameIndex == GETWORDSIZE)
+						{
+							memcpy( recebido,  (recebido+1), 2);
+							recebido[2] = '\0';//isola o id
+							switch (pos)
+							{
+								case 0:
+								strcpy(msgToSend,"OK");
+								break;/*
+								case 1:
+								uint16ToString4(msgToSend,status.freq);
+								break;
+								case 2:
+								uint8ToString4(msgToSend,maxCurrent);
+								break;
+								case 3:
+								uint8ToString4(msgToSend,maxDC);
+								break;
+								case 4:
+								uint8ToString4(msgToSend,minDC);
+								break;
+								case 5:
+								uint8ToString4(msgToSend,maxDV);
+								break;
+								case 6 :
+								if(flags.on)
+								strcpy(msgToSend, "0001");
+								else
+								strcpy(msgToSend, "0000");
+								break;
+								case 7:
+								if(flags.dms)
+								strcpy(msgToSend, "0001");
+								else
+								strcpy(msgToSend, "0000");
+								break;
+								case 8:
+								uint8ToString4(msgToSend,maxTemp);
+								break;
+								case 9:
+								uint8ToString4(msgToSend,minVotage);
+								break;
+								case 10:
+								uint8ToString4(msgToSend,status.dc);
+								break;
+								case 11:
+								uint8ToString4(msgToSend,status.temperature);
+								break;
+								case 12:
+								uint8ToString4(msgToSend,status.current);
+								break;
+								case 13:
+								uint8ToString4(msgToSend,status.voltage);
+								break;*/
+								default:
+								strcpy(msgToSend,"ERRO");
+							}
+							strcat(recebido,msgToSend);
+							strcpy(msgToSend,recebido);
+
+							stringTransmit(msgToSend);
+						}
+						else
+						{
+							if(frameIndex == SETWORDSIZE)
+							{
+								memcpy((void *) recebido, (void *) (recebido+3), 4);//isola somente o valor, usando 4 caracteres
+								recebido[4] = '\0';
+
+								switch (pos)
+								{/*
+									case 1:
+									seta_freq(string4ToUint16(recebido));
+									break;
+									case 2:
+									maxCurrent = string4Touint8(recebido);
+									break;
+									case 3:
+									maxDC = string4Touint8(recebido);
+									break;
+									case 4:
+									minDC = string4Touint8(recebido);
+									estados.estAux1;
+									break;
+									case 5:
+									maxDV = string4Touint8(recebido);
+									break;
+									case 6 :
+									if(recebido[3] == '1')
+									flags.on = 1;
+									if(recebido[3] == '0')
+									flags.on = 0;
+									break;
+									case 7:
+									if(recebido[3] == '1')
+									flags.dms = 1;
+									if(recebido[3] == '0')
+									flags.dms = 0;
+									break;
+									case 8:
+									maxTemp = string4Touint8(recebido);
+									break;
+									case 9:
+									minVotage = string4Touint8(recebido);
+									break;
+									case 10:
+									//seta_dc(string4Touint8(recebido));
+									dcReq = string4Touint8(recebido);
+									break;
+									case 11:
+									case 12:
+									case 13:*/
+
+									default:
+									stringTransmit("ERRO");
+								}
+							}
+							else
+							{
+								stringTransmit("wrong size");
+								esvaziaBuffer();
+							}
+						}
+					}
+					else
+					{//se o inicio da palavra nao esta correto
+						esvaziaBuffer();
+					}
+					frameIndex = 0;
+				}
+			}
+		}
+		//wdt_reset(); //!diz que esta ok para watchdog	
+		_delay_ms(100);
 	}
 	return 0;
+}
+
+/// @Brief Seta o filtro do canBus a ser usado
+/// here...
+/// @param O filtro a ser usado.
+const	uint8_t can_filter[] PROGMEM = {
+	// Group 0
+	MCP2515_FILTER(0),				// Filter 0
+	MCP2515_FILTER(1),				// Filter 1
+	
+	// Group 1
+	MCP2515_FILTER(0),				// Filter 2
+	MCP2515_FILTER(0),				// Filter 3
+	MCP2515_FILTER(0),				// Filter 4
+	MCP2515_FILTER(0),				// Filter 5
+	
+	MCP2515_FILTER(0),				// Mask 0 (for group 0)
+	MCP2515_FILTER(0),				// Mask 1 (for group 1)
+};
+
+
+//envia uma msg usando o protocolo GUI
+void stringTransmit(char* texto)
+{
+	uint8 i = 0;
+	usartTransmit(INICIODOPACOTE);
+	for(i = 0; texto[i] != '\0'; i++)
+	usartTransmit(texto[i]);
+	usartTransmit(FINALDOPACOTE);
+}
+
+//esvazia o buffer de entrada da usart
+void esvaziaBuffer()
+{
+	while(!usartIsReceiverBufferEmpty())
+	usartGetDataFromReceiverBuffer();
 }
